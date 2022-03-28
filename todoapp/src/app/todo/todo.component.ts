76ea98150bc564c71a5a6dc61d6807465ecda277
komalsonar8 <komal.sonar8@gmail.com>
2022-03-28 17:28:36 +0000

    add navbar and todo form component using angular material

---
+++ todoapp/src/app/todo/todo.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder , Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  todoForm!:FormGroup;
+  tasks : any []= [];
+  inprogress : any [] =[];
+  done : any [] = [];
+
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.todoForm=this.fb.group({
+      item:['', Validators.required]
+    })
+
+  }
+
+}
