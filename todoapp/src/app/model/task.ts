5b8503c05682106ddfd539fa4e6307780a0efad8
komalsonar8 <komal.sonar8@gmail.com>
2022-03-29 17:28:29 +0000

    Add tasks , inprogress delete function run

---
+++ todoapp/src/app/model/task.ts
@@ -0,0 +1,4 @@
+export interface ITask{
+    descrption:string,
+    done:boolean
+}
--- todoapp/src/app/todo/todo.component.ts
+++ todoapp/src/app/todo/todo.component.ts
@@ -1,5 +1,7 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder , Validators } from '@angular/forms';
+import { ITask } from '../model/task';
 
 @Component({
   selector: 'app-todo',
@@ -9,9 +11,12 @@ import { FormGroup, FormBuilder , Validators } from '@angular/forms';
 export class TodoComponent implements OnInit {
 
   todoForm!:FormGroup;
-  tasks : any []= [];
-  inprogress : any [] =[];
-  done : any [] = [];
+  tasks : ITask []= [];
+  inprogress : ITask [] =[];
+  done : ITask [] = [];
+  
+  updateId!:any;
+  isEditEnabled:boolean=false;
 
   constructor(private fb:FormBuilder) { }
 
@@ -22,4 +27,46 @@ export class TodoComponent implements OnInit {
 
   }
 
+  addTask(){
+    this.tasks.push({
+      descrption:this.todoForm.value.item,
+      done:false
+    })
+  }
+
+  onEdit(item:ITask, i:number){
+    this.todoForm.controls['item'].setValue(item.descrption);
+    this.updateId=i;
+    this.isEditEnabled=true;
+
+  }
+
+  deleteTask(i:number)
+  {
+    this.tasks.splice(i,1)
+  }
+  deleteinprogressTask(i:number)
+  {
+    this.inprogress.splice(i,1)
+  }
+
+  deletedoneTask(i:number)
+  {
+    this.inprogress.splice(i,1)
+  }
+
+  drop(event: CdkDragDrop<ITask[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
 }
+
